753690f53b6acfac33faa9001aababfa39953d8b
Sherina Buenaseda <sherina76@gmail.com>
2023-06-15 13:21:50 -0600

    completed lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (num1,num2) => num1/num2;
+divide(2000,100);
+
+const square = num1 => num1*num1;
+let num1 = 4;
+square(num1);
+
+const add = (num1,num2) => {
+    console.log(`Adding ${num1}`);
+    console.log(`Adding ${num2}`);
+    return num1+num2;
+}
+add(1,2);
