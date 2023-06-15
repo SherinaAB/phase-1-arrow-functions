6578e8e257452334e9b947cd0622f38131b277bc
Sherina Buenaseda <sherina76@gmail.com>
2023-06-15 13:25:48 -0600

    commit again

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ divide(2000,100);
 
 const square = num1 => num1*num1;
 let num1 = 4;
-square(num1);
+square(num1*num1);
 
-const add = (num1,num2) => {
-    console.log(`Adding ${num1}`);
-    console.log(`Adding ${num2}`);
-    return num1+num2;
-}
+// const add = (num1,num2) => {
+//     console.log(`Adding ${num1}`);
+//     console.log(`Adding ${num2}`);
+//     return num1+num2;
+// }
+// add(1,2);
+
+const add = (num1, num2) => num1 + num2;
 add(1,2);
